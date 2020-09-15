c8db05c83576b935b076ba8f31b7ec3330a297d7
Valentin <primarh111@mail.ru>
2020-09-15 14:35:04 +0300

    sideBar layout

--- exchangerates/src/App.js
+++ exchangerates/src/App.js
@@ -1,15 +1,42 @@
-import React, { Component } from 'react'
-import './App.sass';
-import Layout from './components/layout/Layout';
+import React, { Component } from "react";
+import "./App.sass";
+import Layout from "./components/layout/Layout";
+import RateContext from "./context/RateContext";
+import CHF from "./img/CHF.png";
+import CNY from "./img/CNY.png";
+import EUR from "./img/EUR.png";
+import GBP from "./img/GBP.png";
+import JPY from "./img/JPY.png";
+import RUB from "./img/RUB.png";
+import USD from "./img/USD.png";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      baseCurrency: "USD",
+      rate: "",
+      date: "",
+      currency: {
+        USD: { name: "Доллар США", flag: USD, course: "2.80" },
+        CNY: { name: "Китайский Юань", flag: CNY, course: "9999999" },
+        EUR: { name: "Евро", flag: EUR, course: "9999999" },
+        GBP: { name: "Фунт Стерлингов", flag: GBP, course: "9999999" },
+        JPY: { name: "Японская Йена", flag: JPY, course: "9999999" },
+        RUB: { name: "Российский Рубль", flag: RUB, course: "9999999" },
+        CHF: { name: "Швейцарский Франк", flag: CHF, course: "9999999" },
+      },
+    };
+  }
+
+  
+
   render() {
+    
     return (
-      <div className="App">
-        <Layout/>
-    </div>
-    )
+      <RateContext.Provider value={{state: this.state}}>
+          <Layout />
+      </RateContext.Provider>
+    );
   }
 }
-
-
--- exchangerates/src/components/layout/Layout.js
+++ exchangerates/src/components/layout/Layout.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import './layout.sass'
-import { AddClass } from '../../hoc/AddClass';
-import { Header } from '../header/Header';
+import React from "react";
+import "./layout.sass";
+import { AddClass } from "../../hoc/AddClass";
+import { Header } from "../header/Header";
+import { Home } from "../pages/home/Home";
+import { SideBar } from "../sideBar/SideBar";
 
 const Layout = () => {
-    return (
-        <div>
-            <Header/>
-        </div>
-    )
-}
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="content">
+          <div className='routes'>
+              <Home/>
+          </div>
+         <SideBar/>
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default AddClass(Layout, 'layout')
+export default AddClass(Layout, "layout");
